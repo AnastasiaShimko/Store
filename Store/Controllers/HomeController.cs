f6de2eca6926ba64f858fc178ae0718cd05dafc7
Anastasia Shimko <anastasiaishimko@gmail.com>
2020-12-05 14:28:10 +0300

    Add project files.

---
+++ Store/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        StoreContext db;
+
+        public HomeController(ILogger<HomeController> logger, StoreContext context)
+        {
+            db = context;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Store/Models/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Store.Models
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
