c56da5c8643fa96a7e45c09b102856b63678f8ad
Anastasia Shimko <anastasiaishimko@gmail.com>
2020-12-05 17:32:03 +0300

    SRP example

--- Store/EmailMessageSender.cs
+++ Store/EmailMessageSender.cs
@@ -5,6 +5,10 @@ namespace Store
 {
     public class EmailMessageSender : IMessageSender
     {
+        /// <summary>
+        /// According to SRP, one class should take one responsibility.
+        /// So we should write different class for sending another type of messages (E-mail message).
+        /// </summary>
         public string Send(Order order)
         {
             return $"E-mail was sent to: {order.User.EmailAddress}";
--- Store/TelegramMessageSender.cs
+++ Store/TelegramMessageSender.cs
@@ -9,6 +9,10 @@ namespace Store
 {
     public class TelegramMessageSender : IMessageSender
     {
+        /// <summary>
+        /// According to SRP, one class should take one responsibility.
+        /// So we should write different class for sending another type of messages (Telegram message).
+        /// </summary>
         public string Send(Order order)
         {
             return $"Telegram message was sent to: {order.User.PhoneNumber}";
